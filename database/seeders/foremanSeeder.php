fd3ce584deeac62901e9427a8dbd4e7f71e4cb00
Sameer khan <sameer.k@chapter247.com>
2023-01-03 22:09:22 +0530

    foreman password

--- database/seeders/foremanSeeder.php
+++ database/seeders/foremanSeeder.php
@@ -18,25 +18,25 @@ class foremanSeeder extends Seeder
         $user=User::create([
             'name' => 'nik',
             'email' => 'nikm01@windowslive.com',
-            'password' => bcrypt('Foreman@123'),
+            'password' => bcrypt('BoxIT23'),
         ]);
         $user->assignRole('Foreman');
         $user=User::create([
             'name' => 'dan',
             'email' => 'danpfrancis4@gmail.com',
-            'password' => bcrypt('Foreman@123'),
+            'password' => bcrypt('BoxIT23'),
         ]);
         $user->assignRole('Foreman');
         $user=User::create([
             'name' => 'jimmy',
             'email' => 'jdinwood@outlook.co.nz',
-            'password' => bcrypt('Foreman@123'),
+            'password' => bcrypt('BoxIT23'),
         ]);
         $user->assignRole('Foreman');
         $user=User::create([
             'name' => 'daryl',
             'email' => 'daryl@olsenconstruction.co.nz',
-            'password' => bcrypt('Foreman@123'),
+            'password' => bcrypt('BoxIT23'),
         ]);
         $user->assignRole('Foreman');
     }
