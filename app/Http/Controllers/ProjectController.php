2a262997d750c8c67ffb6455845fd7e8d631d36f
Sameer khan <sameer.k@chapter247.com>
2023-03-04 19:42:13 +0530

    filter work

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -38,23 +38,25 @@ class ProjectController extends Controller
             $months[] = date('F', mktime(0, 0, 0, $m, 1, date('Y')));
         }
         if (!empty(request('q')) || !empty(request('completed_projects')) || !empty(request('month')) || !empty(request('year'))) {
-            DB::enableQueryLog();
 
-            $projects =  Booking::whereHas('BookingData', function ($query) {
-                if (!empty(request('year')))
-                    $query->whereYear('created_at', '=', request('year'));
-                if (!empty(request('month')))
-                    $query->whereMonth('created_at', '=', request('month'));
-            });
-            $projects = $projects->whereHas('PassedProjectStatus', function ($query) {
-                if (!empty(request('completed_projects')))
+            $projects = new Booking;
+
+            if (!empty(request('year')) || (!empty(request('month')))) {
+                $projects =  $projects->whereHas('BookingData', function ($query) {
+                    if (!empty(request('year')))
+                        $query->whereYear('created_at', '=', request('year'));
+                    if (!empty(request('month')))
+                        $query->whereMonth('created_at', '=', request('month'));
+                });
+            }
+            if (!empty(request('completed_projects'))) {
+                $projects = $projects->whereHas('PassedProjectStatus', function ($query) {
                     $query->select(DB::raw('count(*)'))->havingRaw('COUNT(*) = ' . DB::RAW("(SELECT COUNT(*)  FROM `project_status_label` WHERE `department_id` = '' OR `department_id` IN (SELECT department_id  FROM `booking_data` WHERE `booking_id` = `bookings`.`id`))"));
-            });
-
+                });
+            }
             if (!empty(request('q')))
                 $projects = $projects->where('address', 'like', '%' . request('q') . '%');
-           
-                $projects = $projects->get();
+            $projects = $projects->get();
         } else {
             $projects = Booking::all();
         }
