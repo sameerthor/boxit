4dc9986da2e8949bb34d6eb8194815bf29f23057
Sameer khan <sameer.k@chapter247.com>
2023-03-05 21:10:25 +0530

    foreman work

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -811,7 +811,7 @@
 	$(document).on('click', '.save_note', function() {
 		var notes = tinymce.get("note_editor").getContent();
 		var date = $("#foremanModalLabel").find("span").html();
-		var id = $(this).data('id');
+		var id = $(this).attr('data-id');
 		axios.post('/save-foreman-notes', {
 				date: date,
 				id: id,
@@ -841,7 +841,7 @@
 		toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
 		autosave_ask_before_unload: true,
 		image_advtab: true,
-		height: 300,
+		height: 700,
 		image_caption: true,
 		toolbar_mode: 'sliding',
 		contextmenu: 'link image imagetools table configurepermanentpen',
