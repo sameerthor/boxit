d707d8d5d695b087ef7e93231befe146dff3bbfa
Sameer khan <sameer.k@chapter247.com>
2023-05-10 21:27:45 +0530

    placeholder chnage

--- resources/views/new_booking_mail.blade.php
+++ resources/views/new_booking_mail.blade.php
@@ -82,7 +82,7 @@
 								<input type="file" class="form-control col-md-4" multiple>
 							</div>
 							<br>
-							<textarea class="form-control sms_text" placeholder="Put SMS text here.."></textarea>
+							<textarea class="form-control sms_text" placeholder="Please place text message (SMS) content here"></textarea>
 							<br>
 							<div class="email_content" data-subject="{{$res->subject}}" data-id="{{$id}}">
 								<textarea id="textArea{{$loop->iteration}}" class="email_body">{{$res->body}}</textarea>
