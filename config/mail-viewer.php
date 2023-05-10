5b5061d1dce1ab60fb7ead71cf92c5ed6c4c934f
Sameer khan <sameer.k@chapter247.com>
2023-05-10 17:16:52 +0530

    timezone

--- config/mail-viewer.php
+++ config/mail-viewer.php
@@ -4,6 +4,7 @@ return [
 	'table'           => 'mail_logs',
 	'uri_prefix'      => '_mail-viewer',
 	'middleware'      => ['web'],
+	'timezone'=> 'Pacific/Auckland',
 	'date_format' => 'd.m.Y',
 	'time_format' => 'H:i:s',
 	'emails_per_page' => 20,
