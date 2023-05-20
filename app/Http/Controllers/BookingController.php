92644fe1f18e95a6c41ee1315de39e157daf2830
Sameer khan <sameer.k@chapter247.com>
2023-05-20 14:31:30 +0530

    project id added

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -118,7 +118,7 @@ class BookingController extends Controller
                $department=Department::find($key)->title; 
                $email_body="Hi,<br>";
                $email_body.="This is a reminder to check if we have the report for <b>$department</b> at <b>".$request->get('address')."</b>.";
-               $email_body.='<br>Thank You,<br><br><img src="https://boxit.staging.app/img/logo2581-1.png" style="width:75px;height:30px" class="mail-logo" alt="Boxit Logo">';
+               $email_body.='<br><p style="display:none">Project ID #' . $booking_id . '</p>Thank You,<br><br><img src="https://boxit.staging.app/img/logo2581-1.png" style="width:75px;height:30px" class="mail-logo" alt="Boxit Logo">';
            $details['to'] = \config('const.admin1');
            $details['subject'] = 'Booking Cancelled';
            $details['body'] = $email_body;
