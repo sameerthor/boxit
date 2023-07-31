514485c06fa71729f3880eeff1d4fc011d29393d
Sameer khan <sameer.k@chapter247.com>
2023-07-31 10:08:23 +0530

    sso work

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Exception;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Redirect;
 
 class LoginController extends Controller
 {
@@ -36,5 +40,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials,1)) {
+          //  dd(Auth::id());
+            return Redirect::to('https://pm.boxitfoundations.co.nz/proxy-login/'.Auth::id());
+        }
+
+        return redirect("login")->withSuccess('You have entered invalid credentials !');
     }
 }
