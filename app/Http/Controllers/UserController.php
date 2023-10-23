4c0236e2583585ca1ab2598f729f4ad8fee8a5ff
deva2f <dev@a2f.pt>
2023-10-24 00:24:34 +0530

    foreman email work

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,16 +9,17 @@ use Auth;
 use App\Jobs\BookingEmailJob;
 use Carbon\Carbon;
 use Session;
+use Illuminate\Support\Str;
+
 class UserController extends Controller
 {
-    
+
     public function index()
     {
         $users = User::all();
         return view('user', compact('users'));
-    
     }
-  
+
     public function edit_user(Request $request)
     {
         return  User::find($request->get('id'));
@@ -27,37 +28,36 @@ class UserController extends Controller
     public function users(Request $request)
     {
         $search = $request->get('search');
-        $users = User::where('name','like', $search.'%')->get();
+        $users = User::where('name', 'like', $search . '%')->get();
         return view('usertable', compact('users'))->render();
     }
 
     public function add_user(Request $request)
     {
         $user = new User();
-        $user->name=$request->name;
-        $user->password=bcrypt($request->password);
-        $user->email=$request->email;
-        $user->contact=$request->contact;
+        $user->name = $request->name;
+        $user->password = bcrypt($request->password);
+        $user->email = $request->email;
+        $user->contact = $request->contact;
         $user->save();
         $user = $user->fresh();
         $user->assignRole($request->user_type);
         return true;
     }
-    
-    
+
+
 
     public function update_user(Request $request)
     {
         $user = User::find($request->get('id'));
-        $user->name=$request->name;
-        if(!empty($request->password))
-        {
-        $user->password=bcrypt($request->password);
+        $user->name = $request->name;
+        if (!empty($request->password)) {
+            $user->password = bcrypt($request->password);
         }
         $user->roles()->detach();
         $user->assignRole($request->user_type);
-        $user->email=$request->email;
-        $user->contact=$request->contact;
+        $user->email = $request->email;
+        $user->contact = $request->contact;
         $user->save();
 
         return  true;
@@ -72,49 +72,51 @@ class UserController extends Controller
 
     public function mail_user(Request $request)
     {
-        $body=$request->mail;
-        $emails=$request->emails;
-        if(empty($emails))
-        $emails=User::all()->pluck('email');
-        foreach($emails as $res)
-        {
-        $details['to'] = $res;
-        $details['url'] = 'testing';
-        $details['subject'] = 'Notification';
-        $details['body'] = $body;
-        $details['files'] = [];
-        dispatch(new BookingEmailJob($details));
+        $body = $request->mail;
+        $emails = $request->emails;
+        if (empty($emails))
+            $emails = User::all()->pluck('email');
+        foreach ($emails as $res) {
+            $details['to'] = $res;
+            $details['url'] = 'testing';
+            $details['subject'] = 'Notification';
+            $details['body'] = $body;
+            $details['files'] = [];
+            dispatch(new BookingEmailJob($details));
         }
 
         Session::flash('succes_msg', 'Mail has been sent successfuly.');
         return redirect('/user-management');
+    }
 
-
-    }   
-   
-    public function get_leaves(Request $request){
-        $staff_id=$request->get('id');
-        $data=StaffLeave::where('staff_id',$staff_id)->get();
+    public function get_leaves(Request $request)
+    {
+        $staff_id = $request->get('id');
+        $data = StaffLeave::where('staff_id', $staff_id)->get();
         return $data;
     }
 
-    public function save_leaves(Request $request){
-        
-        $staff_id=$request->get('id');
-        StaffLeave::where('staff_id',$staff_id)->delete();
-        $from_dates=$request->get('from_dates');
-        $to_dates=$request->get('to_dates');
-        $i=0;
-        foreach($from_dates as $date)
-        {
-            StaffLeave::create(array('staff_id'=>$staff_id,'to_date'=>date("Y-m-d h:i:s", strtotime($to_dates[$i])),'from_date'=>date("Y-m-d h:i:s", strtotime($date))));
+    public function save_leaves(Request $request)
+    {
+
+        $staff_id = $request->get('id');
+        StaffLeave::where('staff_id', $staff_id)->delete();
+        $from_dates = $request->get('from_dates');
+        $to_dates = $request->get('to_dates');
+        $i = 0;
+        foreach ($from_dates as $date) {
+            StaffLeave::create(array('staff_id' => $staff_id, 'to_date' => date("Y-m-d h:i:s", strtotime($to_dates[$i])), 'from_date' => date("Y-m-d h:i:s", strtotime($date))));
             $i++;
         }
         return true;
-     }
+    }
 
-     public function delete(Request $request){
-          User::find($request->get('id'))->delete();
-          return true;
-     }
+    public function delete(Request $request)
+    {
+        $random = Str::random(8);
+
+        User::where('id', $request->get('id'))->update(['email' => $random . '@boxit.com']);
+        User::find($request->get('id'))->delete();
+        return true;
+    }
 }
